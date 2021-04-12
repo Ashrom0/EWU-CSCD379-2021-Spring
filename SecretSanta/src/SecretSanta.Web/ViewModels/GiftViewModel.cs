4f67cfe0ec309644fe04f574ad1ba9ef4507fd34
Pedro H <phernandez5@eagles.ewu.edu>
2021-04-11 19:08:20 -0700

    updated Assignment2 hw, fixed minor issues

--- SecretSanta/src/SecretSanta.Web/ViewModels/GiftViewModel.cs
+++ SecretSanta/src/SecretSanta.Web/ViewModels/GiftViewModel.cs
@@ -9,7 +9,6 @@ namespace SecretSanta.Web.ViewModels
         public string Title { get; set; } = "";
         public string Description { get; set; } = "";
         public string URL { get; set; } = "";
-        [Display(Name = "URL")]
         public int Priority { get; set; } = 1;  
         public int UserId { get; set; }
     }
