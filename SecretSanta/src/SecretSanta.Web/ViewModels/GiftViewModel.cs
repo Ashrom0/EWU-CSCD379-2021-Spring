82b6be03b7e96e2217de607a762aedf93ff2911b
Pedro H <phernandez5@eagles.ewu.edu>
2021-04-20 13:37:12 -0700

    Fixed the navigation system

--- SecretSanta/src/SecretSanta.Web/ViewModels/GiftViewModel.cs
+++ SecretSanta/src/SecretSanta.Web/ViewModels/GiftViewModel.cs
@@ -6,7 +6,7 @@ namespace SecretSanta.Web.ViewModels
     {
         public int Id { get; set; }
         public string Title { get; set; } = "";
-        public string Description { get; set; } = "";
+        public string? Description { get; set; } = "";
         public string? Url { get; set; } = "";
         public int? Priority { get; set; }
         [Display(Name="User")]
