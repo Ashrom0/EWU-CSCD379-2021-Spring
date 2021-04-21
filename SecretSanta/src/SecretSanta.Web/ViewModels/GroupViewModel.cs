1496e202584a8790c7ddd4fd1cd060e3c5d1e4c2
Pedro H <phernandez5@eagles.ewu.edu>
2021-04-20 18:29:12 -0700

    solved filed requirement issue

--- SecretSanta/src/SecretSanta.Web/ViewModels/GroupViewModel.cs
+++ SecretSanta/src/SecretSanta.Web/ViewModels/GroupViewModel.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
 namespace SecretSanta.Web.ViewModels
 {
     public class GroupViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = "";
     }
 }
