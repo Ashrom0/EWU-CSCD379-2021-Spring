84947180a37d5b4a95607648287f8935771825a4
Pedro H <phernandez5@eagles.ewu.edu>
2021-04-26 17:25:48 -0700

    slight edits

--- SecretSanta/src/SecretSanta.Data/DeleteMe.cs
+++ SecretSanta/src/SecretSanta.Data/DeleteMe.cs
@@ -8,7 +8,9 @@ namespace SecretSanta.Data
         {
             new User() { Id = 1, FirstName = "Keith", LastName = "Emerson" },
             new User() { Id = 2, FirstName = "Ray", LastName = "Manzarek" },
-            new User() { Id = 3, FirstName = "Rick", LastName = "Wakeman" }
+            new User() { Id = 3, FirstName = "Rick", LastName = "Wakeman" },
+            new User() { Id = 4, FirstName = "Mike", LastName = "Oldfield" },
+            new User() { Id = 5, FirstName = "Alan", LastName = "Price" }
         };
     }
 }
